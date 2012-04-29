644d57dba4a5d71be0f1e70a4195840e876f893e
Raphael G Shekwolo <ralphsheks@gmail.com>
2012-04-29 19:25:47 +0100

    deleted index and unused js file

--- app/assets/javascripts/states_list.js
+++
@@ -1,9 +0,0 @@
-var states = new Array("Abia", "Abuja Federal Capital", "Adamawa", "Akwa Ibom", "Anambra", 
-                       "Bauchi", "Bayelsa", "Benue", "Borno", "Cross River", "Delta", 
-                       "Ebonyi", "Edo", "Ekiti", "Enugu", "Gombe", "Imo", "Jigawa", "Kaduna", 
-                       "Kano", "Katsina", "Kebbi", "Kogi", "Kwara", "Lagos", "Nassarawa", 
-                       "Niger", "Ogun", "Ondo", "Osun", "Oyo", "Plateau", "Rivers", "Sokoto", 
-                       "Taraba", "Yobe", "Zamfara");
-
-for(var hi=0; hi<states.length; hi++) 
-document.write("<option value=\""+states[hi]+"\">"+states[hi]+"</option>");
